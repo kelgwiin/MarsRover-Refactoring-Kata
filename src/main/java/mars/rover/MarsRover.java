79078ea356c96d0cf23efcc65547ed524d848216
Kelwin Gamez <kelgwiin@gmail.com>
2020-04-07 21:43:31 +0200

    fix eof

--- src/main/java/mars/rover/MarsRover.java
+++ src/main/java/mars/rover/MarsRover.java
@@ -1,1 +1,25 @@
-package mars.rover;import mars.rover.model.Coordinate;import mars.rover.model.Planet;import mars.rover.rotate.Move;import mars.rover.rotate.MoveBuilder;public class MarsRover {    public static String process(int x, int y, char direction, String instructions) {        MoveBuilder moveBuilder = new MoveBuilder();        Planet planet = new Planet(new Coordinate(x, y, direction));        moveBuilder.build();        instructions.chars().forEach(instruction -> {            Move actionMove;            actionMove = moveBuilder.getActionMove((char) instruction);            Coordinate newPosition;            newPosition = actionMove.doMovement(planet.getPosition());            planet.setPosition(newPosition);        });        return planet.toString();    }}
+package mars.rover;
+
+import mars.rover.model.Coordinate;
+import mars.rover.model.Planet;
+import mars.rover.rotate.Move;
+import mars.rover.rotate.MoveBuilder;
+
+public class MarsRover {
+    public static String process(int x, int y, char direction, String instructions) {
+        MoveBuilder moveBuilder = new MoveBuilder();
+        Planet planet = new Planet(new Coordinate(x, y, direction));
+        moveBuilder.build();
+
+        instructions.chars().forEach(instruction -> {
+            Move actionMove;
+            actionMove = moveBuilder.getActionMove((char) instruction);
+
+            Coordinate newPosition;
+            newPosition = actionMove.doMovement(planet.getPosition());
+            planet.setPosition(newPosition);
+        });
+
+        return planet.toString();
+    }
+}
